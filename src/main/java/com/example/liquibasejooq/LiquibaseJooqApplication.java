9fd2649cde076aa6152f00cca7eb137a8dc3604b
lqlys <lys.lukasz555@gmail.com>
2022-05-01 08:29:15 +0200

    added shedlock

--- src/main/java/com/example/liquibasejooq/LiquibaseJooqApplication.java
+++ src/main/java/com/example/liquibasejooq/LiquibaseJooqApplication.java
@@ -1,9 +1,13 @@
 package com.example.liquibasejooq;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 @EnableTransactionManagement
 @SpringBootApplication
 public class LiquibaseJooqApplication {
---
+++ src/main/java/com/example/liquibasejooq/scheduler/Scheduler.java
@@ -0,0 +1,17 @@
+package com.example.liquibasejooq.scheduler;
+
+import net.javacrumbs.shedlock.core.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+
+
+    @Scheduled(cron = "0/10 * * * * ?")
+    @SchedulerLock(name = "TaskScheduler_scheduledTask",
+            lockAtLeastForString = "PT5S", lockAtMostForString = "PT14S")
+    public void scheduledTask() {
+        System.out.println("aaa");
+    }
+}
